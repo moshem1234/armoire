3259531fb34ffaf91cf3aa1a8f8c243a133ff4ee
moshem1234 <mmoskow1@stevens.edu>
2025-04-07 20:44:14 -0400

    First Commit From XCode

---
+++ Armoire/ArmoireApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArmoireApp.swift
+//  Armoire
+//
+//  Created by Armoire on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ArmoireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
